474e95e64e5694cfd1e61ca939042e9472f0ac4d
hgo150925 <hugo.orlando2030@gmail.com>
2022-07-23 03:38:06 -0300

    nestJS intro

---
+++ 02-nestJS-car-dealership/src/cars/cars.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+@Injectable()
+export class CarsService {
+  private cars = [
+    {
+      id: 1,
+      brand: 'Kia',
+      model: 'Soul',
+    },
+    {
+      id: 2,
+      brand: 'Fiat',
+      model: 'Mobi',
+    },
+    {
+      id: 3,
+      brand: 'Peugeot',
+      model: '3008 SUV',
+    },
+  ];
+
+  findAll() {
+    return this.cars;
+  }
+
+  findOneById(id: number) {
+    const car = this.cars.find((car) => car.id === id);
+
+    if (!car) {
+      throw new NotFoundException(`Car with id '${id}' not found 😅​`);
+    }
+    return car;
+  }
+}
