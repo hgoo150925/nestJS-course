925de532618451e3c50db911fa67ab6ef8ea2a90
hgo150925 <hugo.orlando2030@gmail.com>
2022-07-26 21:17:43 -0300

    creando marcas de los autos

---
+++ 04-nestJS-car-dealership-brands-crud/src/cars/cars.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+
+import { CarsService } from './cars.service';
+import { CreateCarDto } from './dto/create-car.dto';
+import { UpdateCarDto } from './dto/update-car.dto';
+
+// Controller controlan rutas, son los encargados de escuchar la solicitud y emitir una respuesta
+@Controller('cars')
+export class CarsController {
+  // Injeccion de dependencia
+  constructor(private readonly carsService: CarsService) {}
+
+  @Get()
+  getAllCars() {
+    return this.carsService.findAll();
+  }
+
+  @Get(':id')
+  getCarById(@Param('id', new ParseUUIDPipe({ version: '5' })) id: string) {
+    return this.carsService.findOneById(id);
+  }
+
+  @Post()
+  createCar(@Body() createCarDto: CreateCarDto) {
+    return this.carsService.create(createCarDto);
+  }
+
+  @Patch(':id')
+  updateCar(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateCarDto: UpdateCarDto,
+  ) {
+    return this.carsService.update(id, updateCarDto);
+  }
+
+  @Delete(':id')
+  deleteCar(@Param('id', ParseUUIDPipe) id: string) {
+    return this.carsService.delete(id);
+  }
+}
---
+++ 04-nestJS-car-dealership-brands-crud/src/cars/cars.service.ts
@@ -0,0 +1,82 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+
+// import { CreateCarDto } from './dto/create-car.dto';
+// import { UpdateCarDto } from './dto/update-car.dto';
+import { Car } from './interfaces/car.interface';
+import { CreateCarDto, UpdateCarDto } from './dto';
+
+@Injectable()
+export class CarsService {
+  private cars: Car[] = [
+    {
+      id: uuid(),
+      brand: 'Kia',
+      model: 'Soul',
+    },
+    {
+      id: uuid(),
+      brand: 'Fiat',
+      model: 'Mobi',
+    },
+    {
+      id: uuid(),
+      brand: 'Peugeot',
+      model: '3008 SUV',
+    },
+  ];
+
+  findAll() {
+    return this.cars;
+  }
+
+  findOneById(id: string) {
+    const car = this.cars.find((car) => car.id === id);
+
+    if (!car) {
+      throw new NotFoundException(`Car with id '${id}' not found 😅​`);
+    }
+    return car;
+  }
+
+  // Crea un nuevo auto
+  create(createCarDto: CreateCarDto) {
+    const car: Car = {
+      id: uuid(),
+      brand: createCarDto.brand,
+      model: createCarDto.model,
+      // ...createCarDto
+    };
+    this.cars.push(car);
+    return car;
+  }
+
+  // Actualiza un auto
+  update(id: string, updateCarDto: UpdateCarDto) {
+    // validacion de que el id exista
+    let carDB = this.findOneById(id);
+
+    if (updateCarDto.id && updateCarDto.id !== id) {
+      throw new BadRequestException(`Car id: ${id} is not valid`);
+    }
+
+    // generar un nuevo array en el car
+    this.cars = this.cars.map((car) => {
+      if (car.id === id) {
+        carDB = { ...carDB, ...updateCarDto, id };
+        return carDB;
+      }
+      return car;
+    });
+    return carDB;
+  }
+
+  delete(id: string) {
+    const car = this.findOneById(id);
+    this.cars = this.cars.filter((car) => car.id !== id);
+  }
+}
