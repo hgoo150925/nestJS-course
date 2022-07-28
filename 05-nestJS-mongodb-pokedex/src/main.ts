519297c11204cb5bc59164fd1a5aaa69996218aa
hgo150925 <hugo.orlando2030@gmail.com>
2022-07-28 17:02:34 -0300

    agregando prefix a la app

--- 05-nestJS-mongodb-pokedex/src/main.ts
+++ 05-nestJS-mongodb-pokedex/src/main.ts
@@ -5,13 +5,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // https://docs.nestjs.com/faq/global-prefix
+  // Para establecer un prefijo para cada ruta registrada en una aplicación HTTP, utilice el método setGlobalPrefix() de la instancia INestApplication.
   app.setGlobalPrefix('api/v2');
 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      forbidNonWhitelisted: true, 
-    })
+      forbidNonWhitelisted: true,
+    }),
   );
 
   await app.listen(3000);
---
+++ 05-nestJS-mongodb-pokedex/src/pokemon/entities/pokemon.entity.ts
@@ -0,0 +1,2 @@
+// Representacion de una tabla en la DB
+export class Pokemon {}
