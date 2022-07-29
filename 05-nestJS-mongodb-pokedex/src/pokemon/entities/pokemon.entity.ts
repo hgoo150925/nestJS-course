30920b4b76534230622216aaa39dec2d22ff5068
hgo150925 <hugo.orlando2030@gmail.com>
2022-07-29 18:20:50 -0300

    creando conexion a la DB MongoDB

--- 05-nestJS-mongodb-pokedex/src/pokemon/entities/pokemon.entity.ts
+++ 05-nestJS-mongodb-pokedex/src/pokemon/entities/pokemon.entity.ts
@@ -1,2 +1,22 @@
-// Representacion de una tabla en la DB
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+// Representacion de la colecction (tabla) en la DB de MongoDB
+// Document le añade nombres, metodos, etc para trabajar de manera facil
+
+@Schema()
+export class Pokemon extends Document {
+  @Prop({
+    unique: true,
+    id: true,
+  })
+  name: string;
+
+  @Prop({
+    unique: true,
+    id: true,
+  })
+  no: number;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
