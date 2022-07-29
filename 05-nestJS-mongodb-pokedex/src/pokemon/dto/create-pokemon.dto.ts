99dd29a56a7ad4f9dc76cf3af1a2b7a2f25962a8
hgo150925 <hugo.orlando2030@gmail.com>
2022-07-29 18:43:57 -0300

    creando conexion a la DB

--- 05-nestJS-mongodb-pokedex/src/pokemon/dto/create-pokemon.dto.ts
+++ 05-nestJS-mongodb-pokedex/src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,13 @@
-export class CreatePokemonDto {}
+import { IsInt, IsPositive, IsString, Min, MinLength } from 'class-validator';
+
+// CreatePokemonDto es la data que voy a recibir para insertar en la DB
+export class CreatePokemonDto {
+  @IsString()
+  @MinLength(1)
+  name: string;
+
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  no: number;
+}
