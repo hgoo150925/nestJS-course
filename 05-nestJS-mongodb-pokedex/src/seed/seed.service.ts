c4d29d74e9698693260e0c30064dffa80ac6dfcb
hugoorlando <hugo.orlando2030@gmail.com>
2022-08-03 13:40:24 -0300

    creando axios adapter

--- 05-nestJS-mongodb-pokedex/src/seed/seed.service.ts
+++ 05-nestJS-mongodb-pokedex/src/seed/seed.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import axios, { AxiosInstance } from 'axios';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 import { Model } from 'mongoose';
 
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
@@ -8,20 +8,20 @@ import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  private readonly axios: AxiosInstance = axios;
-
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
+
+    private readonly http: AxiosAdapter,
   ) {}
 
   async executeSeed() {
     await this.pokemonModel.deleteMany({}); // delete * from pokemons;
 
-    // La respuesta es de tipo PokeResponse
-    const { data } = await this.axios.get<PokeResponse>(
+    const data = await this.http.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
+
     const pokemonToInsert: { name: string; no: number }[] = [];
 
     data.results.forEach(({ name, url }) => {
